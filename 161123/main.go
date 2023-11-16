742c7f02e1db8cb4e0df7e07da67fdd04f8c8e89
i0xiis <d3bil3khihi@gmail.com>
2023-11-16 16:58:16 +0100

    edited

--- 161123/main.go
+++ 161123/main.go
@@ -9,6 +9,24 @@ import (
 func main() {
 	list := []int{5,8,3,2,9,7,4,1,6,10}
 	for i := 0; i < len(list); i++ {
-		fmt.Printf("%d\n", list[i])
+		bigIdx := -1
+		for j := 0; j < len(list) - i; j++ {
+			if bigIdx == -1 || list[j] > list[bigIdx] {
+				bigIdx = j
+			}
+		}
+		Swap(&list[bigIdx], &list[len(list) - i - 1])
 	}
+	// a := 10
+	// b := -10
+	// Swap(&a, &b)
+	// fmt.Println(a, b)
+
+	fmt.Println(list)
+}
+
+func Swap(x, y *int) {
+	c := *y
+	*y = *x
+	*x = c
 }
